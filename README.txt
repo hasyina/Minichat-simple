7eb2fc8d267b8415367598bbc3ebd9e4d9addde6
hasy <hasyinar@gmail.com>
2017-01-23 16:18:37 +0300

    Création d'un formulaire dans minichat.php

---
+++ README.txt
@@ -0,0 +1,1 @@
+Le projet consiste à créer un minichat avec le langage PHP et  a pour objectif de mettre en pratique les connaissances sur le langage PHP et MYSL
---
+++ minichat.php
@@ -0,0 +1,30 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" href="style.css"/>
+		<title>Mini-chat</title>
+	</head>
+	<body>
+		<div class="corps">
+		
+			<!--Formulaire -->
+			<div id="formulaire">
+				<h1>Mini-Chat</h1>
+				<h3>		
+					<a href="minichat.php">Rafraichir la page</a>
+				</h3>
+				<form method="POST" action="minichat_post.php">
+					<fieldset>
+						<legend>Votre message</legend>
+						<label for="pseudo">Pseudo: </label>
+						<input type="text" name="pseudo" id="pseudo"  /><br/><br/>
+						<label for="message">Message:</label>
+						<input type="text" name="message" id="message"/><br/><br/>
+						<input type="submit" value="Envoyer"/>
+					</fieldset>
+				</form>
+			</div>
+					
+	</body>
+</html>
